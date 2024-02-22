5d99566fa908297083cd6dfa5512a75414a8409c
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-02-22 16:56:01 -0300

    atv 3 incom

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
+Article.create(title: Faker::Games::ClashOfClans.rank, content: Faker::Games::ClashOfClans.troop)
